2c14a22c2c42b7b494663ab4a774db1ae4878f73
sumitcs1312 <sumitcs1312@yahoo.co.in>
2025-05-03 20:24:02 +0530

    Add lambda/pre_signup/index.js 1746284042225

--- lambda/pre_signup/index.js
+++ lambda/pre_signup/index.js
@@ -1,5 +1,6 @@
 exports.handler = async (event) => {
     // Placeholder pre-signup trigger function
     // You can add custom validation or auto-confirm logic here
+    //added my custom logic
     return event;
 };
